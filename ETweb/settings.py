5e4795ad0f82c81350c8555bf1e439c3ea97ca29
Jayaanmol Shah <156838714+JayaanmolShah@users.noreply.github.com>
2024-08-17 19:46:46 +0530

    Add files via upload

--- ETweb/settings.py
+++ ETweb/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure--w%16hz0jq%#gnlqoqn$3n#)l3-klv!q$sv8*lf93lk-)apb(s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1','0.0.0.0:47526','.now.sh','localhost']
 
 
 # Application definition
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'ETweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'CrMSVMPbmIJdWppCzrXOwLqCAzJLactI',
+        'HOST': 'autorack.proxy.rlwy.net',
+        'PORT': '47526',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -114,17 +124,17 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,"static")
-]
-STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles','static')
--- ETweb/urls.py
+++ ETweb/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.site.site_header = "BitesAndBytes Admin"
 admin.site.site_title = "BitesAndBytes Admin Portal"
@@ -26,3 +28,6 @@ urlpatterns = [
     path('',include('home.urls'))
 ]
  
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
