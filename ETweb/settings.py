45132b54cdd360f56ed4d0825e66a0936c2730a5
Jayaanmol Shah <156838714+JayaanmolShah@users.noreply.github.com>
2024-08-18 12:14:42 +0530

    Add files via upload

--- ETweb/settings.py
+++ ETweb/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'ETweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'CrMSVMPbmIJdWppCzrXOwLqCAzJLactI',
-        'HOST': 'autorack.proxy.rlwy.net',
-        'PORT': '47526',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'CrMSVMPbmIJdWppCzrXOwLqCAzJLactI',
+#         'HOST': 'autorack.proxy.rlwy.net',
+#         'PORT': '47526',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
