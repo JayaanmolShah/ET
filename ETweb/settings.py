96b009749559c522005b1d3df5516d2ce74e02dd
Jayaanmol Shah <156838714+JayaanmolShah@users.noreply.github.com>
2024-09-10 00:50:52 +0530

    Add files via upload

--- ETweb/settings.py
+++ ETweb/settings.py
@@ -26,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app','127.0.0.1','0.0.0.0:47526','.now.sh','localhost']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -75,10 +74,21 @@ WSGI_APPLICATION = 'ETweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.wrelzdetluebmnrnuuuj',
+        'PASSWORD': 'W@terbottle@9696',
+        'HOST': 'aws-0-ap-south-1.pooler.supabase.com',
+        'PORT': '6543',
     }
 }
 
